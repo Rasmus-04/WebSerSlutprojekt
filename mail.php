2558c69c2e5d0561dcc0cebf03220fc1aa45d65c
Rasmus <64273761+Rasmus-04@users.noreply.github.com>
2022-12-12 21:34:14 +0100

    Update mail.php

--- mail.php
+++ mail.php
@@ -9,7 +9,7 @@ $mail->isSMTP();
 $mail->Host = 'smtp.gmail.com';
 $mail->SMTPAuth = true;
 $mail->Username = 'slutprojekt.serrestam@gmail.com';
-$mail->Password = 'virxqponzxnclvyj';
+$mail->Password = '';
 $mail->SMTPSecure = "tls";
 $mail->Port = '587';
 
